258180f73837a61aae3060f32db0b65db9042e8f
Clock <waitanboy@gmail.com>
2026-04-17 16:56:14 +0900

    update : cleanup

--- backend/HashGen.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class HashGen {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("admin123"));
-    }
-}
